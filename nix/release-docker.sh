3a750995e7eeb3902b563c7858bf2ed3165ce948
tkachuk-labs <tkachuk.labs@gmail.com>
2022-01-19 23:14:33 +0200

    release improvements WIP

--- nix/release-docker.sh
+++ nix/release-docker.sh
@@ -4,15 +4,5 @@ set -e
 
 THIS_DIR="$(dirname "$(realpath "$0")")"
 
-rm -rf "$THIS_DIR/../build"
-mkdir -p "$THIS_DIR/../build"
-
 sh "$THIS_DIR/shell.sh" --mini \
-   "--run './nix/release-native.sh reckless-trading-bot && \
-   cp -Lr \
-     ./result-docker-image-reckless-trading-bot \
-     ./build/docker-image-reckless-trading-bot.tar.gz && \
-   cp -Lr \
-     ./result-reckless-trading-bot/bin/reckless-trading-bot-exe \
-     ./build/reckless-trading-bot'
-   "
+   "--run './nix/release-native.sh reckless-trading-bot-docker'"
--- nix/release-native.sh
+++ nix/release-native.sh
@@ -3,9 +3,11 @@
 set -e
 
 THIS_DIR="$(dirname "$(realpath "$0")")"
+ROOT_DIR="$THIS_DIR/.."
+BUILD_DIR="$ROOT_DIR/build"
 
 sh \
-  "$THIS_DIR/../reckless-trading-bot/nix/shutdown-test-deps.sh" \
+  "$ROOT_DIR/reckless-trading-bot/nix/shutdown-test-deps.sh" \
   || true
 
 build () {
@@ -21,13 +23,27 @@ build_bitfinex_client () {
     bitfinex-client.components.library
 }
 
-build_reckless_trading_bot () {
+
+build_reckless_trading_bot_native () {
   build \
     reckless-trading-bot \
     reckless-trading-bot.components.exes.reckless-trading-bot-exe
-  nix-build "$THIS_DIR/../reckless-trading-bot/nix/docker.nix" \
+  rm -rf \
+    "$BUILD_DIR/reckless-trading-bot"
+  cp -Lr \
+    "$ROOT_DIR/result-reckless-trading-bot/bin/reckless-trading-bot-exe" \
+    "$BUILD_DIR/reckless-trading-bot"
+}
+
+build_reckless_trading_bot_docker () {
+  nix-build "$ROOT_DIR/reckless-trading-bot/nix/docker.nix" \
     -v --show-trace \
     --out-link "result-docker-image-reckless-trading-bot"
+  rm -rf \
+    "$BUILD_DIR/docker-image-reckless-trading-bot.tar.gz"
+  cp -Lr \
+    "$ROOT_DIR/result-docker-image-reckless-trading-bot" \
+    "$BUILD_DIR/docker-image-reckless-trading-bot.tar.gz"
 }
 
 TARGET="$1"
@@ -36,13 +52,18 @@ case $1 in
     build_bitfinex_client
     shift
     ;;
-  reckless-trading-bot)
-    build_reckless_trading_bot
+  reckless-trading-bot-native)
+    build_reckless_trading_bot_native
+    shift
+    ;;
+  reckless-trading-bot-docker)
+    build_reckless_trading_bot_docker
     shift
     ;;
   all)
     build_bitfinex_client
-    build_reckless_trading_bot
+    build_reckless_trading_bot_native
+    build_reckless_trading_bot_docker
     shift
     ;;
   *)
