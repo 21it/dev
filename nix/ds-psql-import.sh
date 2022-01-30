165770b85eed53cb2eef5bcebe1a48a4a9c3bf11
tkachuk-labs <tkachuk.labs@gmail.com>
2022-01-30 20:40:25 +0200

    fix psql import

--- nix/ds-psql-import.sh
+++ nix/ds-psql-import.sh
@@ -4,8 +4,7 @@ set -e
 
 THIS_DIR="$(dirname "$(realpath "$0")")"
 BUILD_DIR="$THIS_DIR/../build"
-PSQL_DIR="/var/lib/postgresql"
-DATA_DIR="$PSQL_DIR/data"
+DATA_DIR="/var/lib/postgresql/data"
 
 sh "$THIS_DIR/ds-down.sh" || true
 
@@ -15,7 +14,7 @@ docker run --rm \
   -w /app heathmont/postgres:11-alpine-a2e8bbe \
   sh -c "
   mkdir -p $DATA_DIR &&
-  tar -xvzf ./data.tar.gz -C $PSQL_DIR
+  tar -xvzf ./data.tar.gz -C /
   "
 
 echo "21it_postgres ==> imported from $BUILD_DIR/data.tar.gz"
